5cc3b8aadf87130f1913f2ef6381481011a941af
Gnanamanikanti <gnana.manikanti143@gmail.com>
2026-04-08 15:54:27 -0500

    Clean up shippingservice tracing message

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func init() {
 func main() {
 	if os.Getenv("DISABLE_TRACING") == "" {
 		log.Info("Tracing enabled, but temporarily unavailable")
-		log.Info("See https://github.com/GoogleCloudPlatform/microservices-demo/issues/422 for more info.")
+		log.Info("Tracing support is not fully implemented in this service yet.")
 		go initTracing()
 	} else {
 		log.Info("Tracing disabled.")
